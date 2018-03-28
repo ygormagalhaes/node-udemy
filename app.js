05970e4c44e7adddd98b4c89e643657af0483a52
Ygor Magalhães <emaildoygor@gmail.com>
2018-03-28 15:04:53 -0300

    Separando as configurações do servidor em um arquivo / módulo específico

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
 app.get('/', (req, res) => {
 
---
+++ config/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+module.exports = app;
