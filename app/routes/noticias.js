212d8b25f773aa0152d94f1640c0f239db0f8b03
Ygor Magalhães <emaildoygor@gmail.com>
2018-03-28 18:27:01 -0300

    Refactoring da conexão com o banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,9 +2,11 @@ module.exports = (app) => {
 
     app.get('/noticias', (req, res) => {
 
+        var connection = app.connection();
+
         var consulta = 'SELECT * FROM noticias';
 
-        app.connection.query(consulta, (error, results) => {
+        connection.query(consulta, (error, results) => {
 
             res.render('noticias/noticias', {noticias: results});
 
