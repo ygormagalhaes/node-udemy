5073ad66cbee9a260c9ef4f5dcfa5d9cffed30a5
Ygor Magalhães <emaildoygor@gmail.com>
2018-03-28 14:46:26 -0300

    Inclusão de novas rotas para a aplicação

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.render('secao/home');
+    res.render('home/index');
 
 });
 
-app.get('/skate', (req, res) => {
+app.get('/form-noticia', (req, res) => {
     
-        res.send('O mundo do skate!');
+    res.render('admin/form_add_noticia');
+
+});
+
+app.get('/noticias', (req, res) => {
     
-    });
+    res.render('noticias/noticias');
+
+});
 
 app.listen('3000', () => {
 
