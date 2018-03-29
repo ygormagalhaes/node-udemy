61213395f53c7bd7b73952c3a8c9471dfdddbec4
Ygor Magalhães <emaildoygor@gmail.com>
2018-03-29 12:27:30 -0300

    Instalação e utilização do consign para carregar as rotas automaticamente e refactoring da instancia da função de conexão com bd para o arquivo server

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 var app = require('./config/server');
 
-app.connection = require('./config/connection');
-
-var routeHome = require('./app/routes/home')(app);
-
-var routeFormNoticia = require('./app/routes/form-noticia')(app);
-
-var routeNoticias = require('./app/routes/noticias')(app);
-
 app.listen('3000', () => {
 
     console.log('Aplicação node iniciada!');
--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var consign = require('consign');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+consign().include('./app/routes').into(app);
+
+app.connection = require('../config/connection');
 
 module.exports = app;
