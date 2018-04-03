409a534ba6070c0340f9632dc5420d4ec7d45023
Ygor Magalhães <emaildoygor@gmail.com>
2018-04-03 16:06:07 -0300

    Instalação do express-validator

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator);
 
 consign()
     .include('./app/routes')
